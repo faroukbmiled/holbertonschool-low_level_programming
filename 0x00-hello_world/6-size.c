647f74d8b89f5d9e21c1ac9d726be246e479d169
faroukbmiled <fcfaroukfc@gmail.com>
2022-02-03 06:23:52 -0800

    attempt

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ long int longintType;
 long long int longlongint;
 float floatType;
 
-printf("Size of char: %zu byte\n", sizeof(charType));
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of double: %zu bytes\n", sizeof(longintType));
-printf("Size of double: %zu bytes\n", sizeof(longlongint));
-printf("Size of float: %zu bytes\n", sizeof(floatType));
+printf("Size of a char: %zu byte\n", sizeof(charType));
+printf("Size of an int: %zu bytes\n", sizeof(intType));
+printf("Size of a long int: %zu bytes\n", sizeof(longintType));
+printf("Size of a long long int: %zu bytes\n", sizeof(longlongint));
+printf("Size of a float: %zu bytes\n", sizeof(floatType));
 return (0);
 }
