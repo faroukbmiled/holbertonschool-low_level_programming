7ec1181caab4e97679dd10638c662b64f01924b9
faroukbmiled <fcfaroukfc@gmail.com>
2022-02-03 13:35:28 -0800

    test2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-    fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-    getchar();
-    return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+getchar();
+return (1);
 }
