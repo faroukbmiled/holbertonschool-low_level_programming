412d3f1502c09cf15295588c6e3ac2430de47fcc
faroukbmiled <fcfaroukfc@gmail.com>
2022-02-09 08:55:26 -0800

    fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,9 +7,9 @@
 void jack_bauer(void)
 {
 int a, b;
-for(a = 0; a < 24; ++a)
+for (a = 0; a < 24; ++a)
 {
-for(b = 0; b < 60; ++b)
+for (b = 0; b < 60; ++b)
 {
 _putchar(a / 10 + '0');
 _putchar(a % 10 + '0');
