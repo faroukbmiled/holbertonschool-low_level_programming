aa333a7df538982a045ea5652769dcf0c9e46eb6
faroukbmiled <fcfaroukfc@gmail.com>
2022-02-10 09:25:50 -0800

    initial

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+* print_diagonal -diagonal line.
+* @n: integer.
+* Return:line.
+*/
+void print_diagonal(int n)
+{
+int i, b;
+
+i = 0;
+if (n > 0)
+{
+while (i < n)
+{
+b = 0;
+while (b < i)
+{
+_putchar(' ');
+b++;
+}
+b++;
+_putchar('\\');
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
