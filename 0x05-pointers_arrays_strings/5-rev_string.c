f851e1af3e46eba13aceb837d4173a4033c44908
faroukbmiled <fcfaroukfc@gmail.com>
2022-02-14 13:40:03 -0800

    fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,20 +6,24 @@
  */
 void rev_string(char *s)
 {
-int a, x, b, v;
-while (s[a] != '\0')
-{
-a++;
-}
-v = a;
-a = 0;
-x = v - 1;
-while (a < x)
-{
-b = s[a];
-s[a] = s[x];
-s[x] = b;
-a++;
-x--;
-}
+	int i, b, c, v;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	v = i;
+	i = 0;
+	c = v - 1;
+
+	while (i < c)
+	{
+		c = s[i];
+		s[i] = s[b];
+		s[b] = c;
+		i++;
+		b--;
+	}
+
 }
