6a7967f29ac9082a978c7e083b0f51383a2f33ed
faroukbmiled <fcfaroukfc@gmail.com>
2022-02-14 02:13:19 -0800

    initial

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* _strlen -returns the length of a string
+* @s: variable
+* Return: void
+*/
+int _strlen(char *s)
+{
+int len
+char string[] = *s
+len = strlen(string);
+}
