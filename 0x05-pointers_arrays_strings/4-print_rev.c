7daf8c31b47505c66639b6ae26a0962e6f477577
faroukbmiled <fcfaroukfc@gmail.com>
2022-02-14 03:06:54 -0800

    update

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_rev -prints a string, in reverse, followed by a new line
+* @s: char
+* Return: void
+*/
+void print_rev(char *s)
+{
+int l;
+for (len = 0; s[l] != '\0'; l++)
+{
+_putchar(str[l]);
+}
+_putchar('\n');
+return;
+}
