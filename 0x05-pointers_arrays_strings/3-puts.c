670913e5b4d8859071687098bcdcc1f3cc563f34
faroukbmiled <fcfaroukfc@gmail.com>
2022-02-14 02:50:42 -0800

    initial

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _puts -prints a string, followed by a new linexã
+* @str: char
+* Return: void
+*/
+void _puts(char *str)
+{
+int len;
+for (len = 0; s[len] != '\0'; ++len)
+;
+{
+_putchar(s[len])
+}
+_putchar('\n');
+}
