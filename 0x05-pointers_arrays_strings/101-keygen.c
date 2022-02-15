0adf42225b78ad5ad16afdfe0ad9ee91b28fc626
faroukbmiled <fcfaroukfc@gmail.com>
2022-02-15 05:44:35 -0800

    testsample2

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,24 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <time.h>
 
 int main()
 {
+    int i = 0;
+    int n = 0;
+    int randomizer = 0;
     srand((unsigned int)(time(NULL)));
-    int i;
-    char pass[12];
-
-    printf("Press enter to get a twelve-character password\n");
-    getchar();
-
-    for (i = 0; i < 4; i++) {
-        pass[i] = rand() % 9;
-        char capLetter = 'A' + (rand() % 26);
-        pass[i + 2] = capLetter;
-        char letter = 'a' + (rand() % 26);
-        pass[i + 3] = letter;
-        printf("%d%c%c", pass[i], pass[i + 2], pass[i + 3]);
+    char numbers [] = "1234567890";
+    char letter [] = "abcdefghijklmnoqprstuvwyzx";
+    char letterr [] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+    char symbols [] = "!@#$%^&*(){}[]:<>?,./";
+    printf("\nHow long password:");
+    scanf("%d", &n);
+    char password[n];
+    randomizer = rand() % 4;
+    for (i=0;i<n;i++)
+    {
+        if(randomizer == 1)
+        {
+            password[i] = numbers[rand() % 10];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+        else if (randomizer == 2)
+        {
+            password[i] = symbols[rand() % 26];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+        else if (randomizer == 3)
+        {
+            password[i] = letterr[rand() % 26];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+        else
+        {
+            password[i] = letter[rand() % 21];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
     }
-    printf("\n\n");
+    return main();
 }
