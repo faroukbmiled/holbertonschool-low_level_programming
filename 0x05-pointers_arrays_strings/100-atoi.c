d429b26ba27767bd32d7a13f6d62629e023f3e0f
faroukbmiled <fcfaroukfc@gmail.com>
2022-02-15 11:20:15 -0800

    string to an integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _atoi - Convert a string to an integer.
+ * @s: The pointer to convert
+ * Return: A integer
+ */
+int _atoi(char *s)
+{
+	int sg = 1;
+	unsigned int t = 0;
+	char chr = 0;
+
+	while (*s)
+	{
+		if (*s == '-')
+			sg = sg * -1;
+		if (*s >= '0' && *s <= '9')
+		{
+			chr = 1;
+			t = t * 10 + *s - '0';
+		}
+        else if (chr)
+			break;
+        s++;
+    }
+
+	if (sg < 0)
+		t = (-t);
+
+	return (t);
+}
