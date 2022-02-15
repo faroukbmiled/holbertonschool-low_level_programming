efe2a0b1623bbcf55edf0710564ac1ee648d7ca3
faroukbmiled <fcfaroukfc@gmail.com>
2022-02-15 11:21:55 -0800

    string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,26 +6,23 @@
  */
 int _atoi(char *s)
 {
-	int sg = 1;
-	unsigned int t = 0;
-	char chr = 0;
-
-	while (*s)
-	{
-		if (*s == '-')
-			sg = sg * -1;
-		if (*s >= '0' && *s <= '9')
-		{
-			chr = 1;
-			t = t * 10 + *s - '0';
-		}
-        else if (chr)
-			break;
-        s++;
-    }
-
-	if (sg < 0)
-		t = (-t);
-
-	return (t);
+int sg = 1;
+unsigned int t = 0;
+char chr = 0;
+while (*s)
+{
+if (*s == '-')
+sg = sg * -1;
+if (*s >= '0' && *s <= '9')
+{
+chr = 1;
+t = t * 10 + *s - '0';
+}
+else if (chr)
+break;
+s++;
+}
+if (sg < 0)
+t = (-t);
+return (t);
 }
