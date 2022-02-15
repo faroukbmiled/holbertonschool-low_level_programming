51d1417aa678ea4b7bafb457fb4f3b12ecf0c3c6
faroukbmiled <fcfaroukfc@gmail.com>
2022-02-15 05:45:55 -0800

    sample3

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,47 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
-
-int main()
-{
-    int i = 0;
-    int n = 0;
-    int randomizer = 0;
-    srand((unsigned int)(time(NULL)));
-    char numbers [] = "1234567890";
-    char letter [] = "abcdefghijklmnoqprstuvwyzx";
-    char letterr [] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-    char symbols [] = "!@#$%^&*(){}[]:<>?,./";
-    printf("\nHow long password:");
-    scanf("%d", &n);
-    char password[n];
-    randomizer = rand() % 4;
-    for (i=0;i<n;i++)
-    {
-        if(randomizer == 1)
-        {
-            password[i] = numbers[rand() % 10];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else if (randomizer == 2)
-        {
-            password[i] = symbols[rand() % 26];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else if (randomizer == 3)
-        {
-            password[i] = letterr[rand() % 26];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else
-        {
-            password[i] = letter[rand() % 21];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-    }
-    return main();
+#include <stdlib.h>
+int password(int password_length) {
+char list[] = "1234567890qwertyuiopasdfghjklzxcvbnm!@#$%^&*()_- +=QWERTYUIOPASDFGHJKLZXCVBNM[]{};':\"<>,.?/\|";
+printf("\t");
+for(int i = 0; i < password_length; i++) {
+    printf("*");
+}
+printf("\n");
+printf("\t");
+srand(time(NULL));
+for(int i = 0; i < password_length; i++) {
+    printf("%c", list[rand() % (sizeof list - 1)]);
+}
+printf("\n");
+printf("\t");
+for(int i = 0; i < password_length; i++) {
+    printf("*");
+}
+printf("\n");
+}
+int main() {
+int password_length;
+printf("\n\t*********************************\n\n");
+printf("\tWelcome to the password generator\n\n");
+printf("\t*********************************\n");
+printf("\n\tEnter length of the password = ");
+scanf("%d", &password_length);
+printf("\n");
+printf("\n");
+password(password_length);
+return 0;
 }
