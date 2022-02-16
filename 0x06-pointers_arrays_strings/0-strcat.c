aa266f07f80ae945fd73114a93e5918e7596321d
faroukbmiled <fcfaroukfc@gmail.com>
2022-02-16 01:37:01 -0800

    initial

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcat -function that concatenates 2 strings
+ * @dest: first string
+ * @src: second string
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+int i, x;
+i = 0
+while (dest[i] != '\0')
+{
+i++
+}
+x = 0
+while (src[x] != '\0')
+{
+x++    
+}
+dest[i] = src[x]
+}
+return (dest);
+}
