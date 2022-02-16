b6ed545f942c47262db83a2705bf042052f37799
faroukbmiled <fcfaroukfc@gmail.com>
2022-02-16 02:11:08 -0800

    success

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,18 +6,19 @@
  * Return: dest
  */
 char *_strcat(char *dest, char *src)
-int i, x;
-i = 0
-x = 0
+{
+int i ,x;
+i = 0;
+x = 0;
 while (dest[i] != '\0')
 {
-i++
+i++;
 }
 while (src[x] != '\0')
 {
-x++    
-}
-dest[i] = src[x]
+dest[i] = src[x];
+x++;
+i++;
 }
 return (dest);
 }
