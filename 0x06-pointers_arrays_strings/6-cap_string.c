33bcbcb6637b5321f1071c141d355103b3f7ad20
faroukbmiled <fcfaroukfc@gmail.com>
2022-02-17 15:51:45 -0800

    cap,string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,1 +1,30 @@
+#include "main.h"
 
+/**
+ * cap_string - capitalizes all words in a string
+ * @str: string
+ * Return: address of s
+ */
+char *cap_string(char *str)
+{
+int i = 0, x;
+char c[] = " \t\n,;.!?\"(){}";
+while (str[i])
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+if (i == 0)
+str[i] = str[i] - 32;
+else
+{
+for (x = 0; x <= 12; x++)
+{
+if (c[x] == (str[i] - 1))
+str[i] = str[i] - 32;
+}
+}
+}
+i++;
+}
+return (str);
+}
