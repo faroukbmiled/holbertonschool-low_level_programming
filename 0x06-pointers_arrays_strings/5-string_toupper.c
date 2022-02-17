255b752fe4043657e29919815cff4f8741cb0cdc
faroukbmiled <fcfaroukfc@gmail.com>
2022-02-17 14:49:50 -0800

    attempt

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * string_toupper - lowercase letters of a string to uppercase
+ * @string:  string
+ * Return: change for uppercase
+ */
+char *string_toupper(char *string)
+{
+int i;
+for (i = 0;  string[i] != '\0'; i++)
+{
+if (string[i] >= 'a' && string[i] <= 'z')
+string[i] = ('a' - 'A');
+i++;
+}
+return (string);
+}
