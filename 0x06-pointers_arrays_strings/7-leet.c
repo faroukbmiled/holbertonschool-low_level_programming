b30630afd1fcf95446373d37c4c2eb6c6fd20d83
faroukbmiled <fcfaroukfc@gmail.com>
2022-02-17 15:23:58 -0800

    wrong file lmao

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * leet - encodes a string
+ * @str: the string to encode
+ *
+ * Return: the encode string
+ */
+char *leet(char *string)
+{
+int i, x;
+char l[] = "aAeEoOtTlL";
+char n[] = "4433007711";
+i = 0;
+x = 0;
+while (string[i] != '\0')
+{
+while (l[x] != '\0')
+{
+if (string[i] == l[x])
+{
+string[i] = n[x];
+}
+x++;
+}
+i++;
+}
+return (string);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -14,14 +21,6 @@ int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-#include <unistd.h>
-
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
@@ -44,7 +43,7 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
-char *cap_string(char *);
+char *cap_string(char *s);
 char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
