b9ddb66e93be87e13f7739b8b52323f95a40ebfe
faroukbmiled <fcfaroukfc@gmail.com>
2022-02-17 16:49:27 -0800

    quick fix

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,7 +10,7 @@ int i, x;
 char c[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 i = 0;
-for (i = 0;  string[i] != '\0'; i++)
+while (string[i] != '\0')
 {
 x = 0;
 while (c[x] != '\0')
