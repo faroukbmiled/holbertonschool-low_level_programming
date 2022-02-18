0dffd5b466dd1dd7e167dfbb3a5a1799f0f454e4
faroukbmiled <fcfaroukfc@gmail.com>
2022-02-17 16:31:34 -0800

    initial test

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @string: the string to encode
+ * Return: encode string
+ */
+char *rot13(char *string)
+{
+int i, x;
+x = 0;
+i = 0;
+char c[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+for (i = 0;  string[i] != '\0'; i++)
+{
+while (c[x] != '\0')
+{
+if (string[i] == c[x])
+{
+string[i] = rot13[x];
+break;
+}
+x++;
+}
+i++;
+}
+return (string);
+}
