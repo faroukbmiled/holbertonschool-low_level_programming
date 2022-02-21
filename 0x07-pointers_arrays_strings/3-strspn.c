cb1372e4ad333d20ae319e40a9e76763407ee83a
faroukbmiled <fcfaroukfc@gmail.com>
2022-02-21 14:06:14 -0800

    checker?

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,6 +3,7 @@
  * _strspn - gets the length of a prefix substring
  * @s: pointer to a string
  * @accept: pointer to a string
+ *
  * Return: the number of bytes
  */
 unsigned int _strspn(char *s, char *accept)
