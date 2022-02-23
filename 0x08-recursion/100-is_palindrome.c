a9786633fe8df91b89d006589ef99648c0b254af
faroukbmiled <fcfaroukfc@gmail.com>
2022-02-23 12:16:30 -0800

    update

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,7 +2,7 @@
 /**
  * is_palindrome - find a palindrome string
  * @s: string for check
- * Return: 1 if is palindrome, otherwise 0
+ * Return: 1 if is palindrome otherwise 0
  */
 int is_palindrome(char *s)
 {
