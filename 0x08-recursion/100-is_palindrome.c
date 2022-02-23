06151b61eb9db38ffed135efa106108053cb1920
faroukbmiled <fcfaroukfc@gmail.com>
2022-02-23 12:13:42 -0800

    update

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * is_palindrome - find a palindrome string
+ * @s: string for check
+ * Return: 1 if is palindrome, otherwise 0
+ */
+int is_palindrome(char *s)
+{
+int x;
+x = 0;
+if (*s == x)
+}
+{
+return (0);
+return (1);
+}
+}
