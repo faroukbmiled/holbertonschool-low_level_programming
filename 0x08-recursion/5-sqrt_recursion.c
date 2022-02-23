eb67b6850ef4e03704bb281816df95c019ceb962
faroukbmiled <fcfaroukfc@gmail.com>
2022-02-23 15:57:13 -0800

    maybe?

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,13 +5,13 @@
  * @b: The number to test for the square root of @a
  * Return: square root
  */
-int _sqrt_recursion(int a ,int b)
+int checking(int a ,int b)
 {
 if (b * b > a)
 return (-1);
 else if (b * b == a)
 return (b);
 else
-return (_sqrt_recursion(a, b + 1));
+return (checking(a, b + 1));
 return (1);
 }
