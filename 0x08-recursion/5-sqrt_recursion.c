0faf00206c9677e24b88f12eea5c8871de09175d
faroukbmiled <fcfaroukfc@gmail.com>
2022-02-23 16:00:09 -0800

    maybe?

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,17 +1,24 @@
 #include "main.h"
+
 /**
- * checking - Finds the square root of two numbers
+ * check - Finds the square root of two numbers
+ *
  * @a: The number
  * @b: The number to test for the square root of @a
+ *
  * Return: square root
  */
-int checking(int a ,int b)
+
+int check(int a, int b)
 {
-if (b * b > a)
-return (-1);
-else if (b * b == a)
-return (b);
-else
-return (checking(a, b + 1));
-return (1);
+	if (b * b > a)
+		return (-1);
+
+	else if (b * b == a)
+		return (b);
+
+	else
+		return (check(a, b + 1));
+
+	return (1);
 }
