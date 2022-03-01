63385848001e01c4fc3fc7b25854a558aee666e6
faroukbmiled <fcfaroukfc@gmail.com>
2022-03-01 14:01:48 -0800

    test

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - create a string
+ *@str:string
+ *Return: 0
+ */
+char *_strdup(char *str)
+{
+char *chr;
+int i, x;
+x = 0:
+if (str == NULL)
+return (NULL);
+while (str[x] != '\0')
+x++;
+chr = malloc(x + 1);
+if (chr == NULL)
+return (NULL);
+for (i = 0; i < x; i++)
+chr[i] = *(str + i);
+return (chr);
+}
