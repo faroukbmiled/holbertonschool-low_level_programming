7e98678d5952886366e0c6ab680ed88ebf11cbec
faroukbmiled <fcfaroukfc@gmail.com>
2022-03-01 13:55:34 -0800

    initial

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - create an array
+ *@size:unsigned int
+ *@c:char
+ *Return: 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *chr;
+	unsigned int i;
+	if (size == 0)
+		return (NULL);
+	chr = malloc(size * sizeof(c));
+	if (chr == NULL)
+		return (0);
+	for (i = 0; i <= size; i++)
+	{
+		chr[i] = c;
+	}
+	return (chr);
+}
