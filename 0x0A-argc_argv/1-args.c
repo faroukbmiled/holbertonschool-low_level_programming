a9955014477411114f21a4219acafe2cbf79a617
faroukbmiled <fcfaroukfc@gmail.com>
2022-03-02 10:35:25 -0800

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - function that prints the number of arguments passed into it.
+*@argc: argument count.
+*@argv: argument vector.
+*Return: 0
+*/
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
