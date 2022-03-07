5a2a628b52d4426981e663b90d34f39c27a71af7
faroukbmiled <fcfaroukfc@gmail.com>
2022-03-07 13:26:46 -0800

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _dog_h
+#define _dog_h
+/**
+ *struct dog - print describe dog
+ *@name: char
+ *@age:float
+ *@owner:char
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
