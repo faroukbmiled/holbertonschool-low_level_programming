720cc0022e76e21f77aa53641379acc100154f34
faroukbmiled <fcfaroukfc@gmail.com>
2022-03-09 14:21:31 -0800

    index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+/**
+ *int_index - print the index of the array
+ *@array:int
+ *@size:size if arrays
+ * @array: array of elements
+ * Return: return the index of the first element
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int x;
+if (array == NULL || cmp == NULL || size <= 0)
+{
+return (-1);
+}
+for (x = 0 ; x < size ; x++)
+{
+if ((*cmp)(array[x]) != 0)
+{
+return (x);
+}
+}
+return (-1);
+}
