40f646879ee53c1288fb41fac412a09df3bd55d5
faroukbmiled <fcfaroukfc@gmail.com>
2022-03-09 14:48:31 -0800

    update

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,5 @@
 #include "3-calc.h"
-#include <stdlib.h>
-
+#include <stddef.h>
 /**
  * get_op_func - Selects the correct function to perform
  *               the operation asked by the user.
@@ -19,10 +18,13 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i = 0;
+	int i;
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	while (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
 		i++;
-
-	return (ops[i].f);
+	}
+	return (NULL);
 }
