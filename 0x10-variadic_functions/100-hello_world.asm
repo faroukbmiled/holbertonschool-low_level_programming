bf6f0f1a0aff421ed631bc80bc5093b229554ce2
faroukbmiled <fcfaroukfc@gmail.com>
2022-03-10 14:13:47 -0800

    fix

--- 0x10-variadic_functions/100-hello_world.asm
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -1,13 +1,10 @@
 section .data
-		message db "Hello, Holberton",10
-
-		section .text
-		global main
-
+message db "Hello, Holberton",10
+section .text
+global main
 main:
-		mov rdi, 1
-		mov rsi, message
-		mov rax, 1
-		mov rdx, 17
-		syscall
-        
+mov rdi, 1
+mov rsi, message
+mov rax, 1
+mov rdx, 17
+syscall
