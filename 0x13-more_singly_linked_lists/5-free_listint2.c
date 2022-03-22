2c3b7dcb7cd1a770a22dd8cd778f9c8e36a069e0
Ryuk <51106560+faroukbmiled@users.noreply.github.com>
2022-03-22 23:41:07 +0100

    free list - sets head

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a list and sets head to NULL
+ * @head: pointer to pointer of head
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *n;
+if (head == NULL)
+return;
+while (*head)
+{
+n = (*head)->n;
+free(*head);
+*head = n;
+}
+*head = NULL;
+}
