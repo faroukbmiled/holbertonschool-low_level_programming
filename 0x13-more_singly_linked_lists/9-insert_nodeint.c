dc473f251c9517b0c64719362433dc3796d3199d
faroukbmiled <fcfaroukfc@gmail.com>
2022-03-22 16:15:46 -0700

    quick fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,7 +2,7 @@
 /**
  * insert_nodeint_at_index - inserts a new node at a given index
  * @head: a pointer to list
- * @index: the given index
+ * @idx: the given index
  * @n: the given number
  * Return: a pointer to the newly add node
  */
@@ -15,7 +15,7 @@ if (!new)
 return (NULL);
 new->n = n;
 if (idx == 0)
-{		
+{
 new->next = *head;
 *head = new;
 return (new);
@@ -24,11 +24,11 @@ while (temp && i < idx)
 {
 new->next = temp->next;
 if (i == idx - 1)
-{		
+{
 temp->next = new;
 return (new);
 }
-else		
+else
 temp = temp->next;
 i++;
 }
