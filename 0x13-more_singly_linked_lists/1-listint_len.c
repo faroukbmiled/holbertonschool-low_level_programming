9b52e63ceb3f6b33a137376478a9df833fc3f867
Ryuk <51106560+faroukbmiled@users.noreply.github.com>
2022-03-22 23:27:15 +0100

    list len

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * listint_len - returns number of elements.
+ * @h: pointer to list
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+size_t count = 0;
+while (h)
+{
+h = h->next;
+count++;
+}
+return (count);
+}
