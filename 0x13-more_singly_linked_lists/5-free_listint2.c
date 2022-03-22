fe9bb6e8c483023f6811cff81bbd0dd5cc40893e
Ryuk <51106560+faroukbmiled@users.noreply.github.com>
2022-03-22 23:48:06 +0100

    fix?

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,9 +9,9 @@ void free_listint2(listint_t **head)
 listint_t *n;
 if (head == NULL)
 return;
-while (*head)
+while (*head != NULL)
 {
-n = (*head)->n;
+n = (*head)->next;
 free(*head);
 *head = n;
 }
